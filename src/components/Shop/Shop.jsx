42acd165258f59edbbccfbb6f731cb76c8fe2215
Eriwe-li <11926638+zakingslayerv22@users.noreply.github.com>
2026-01-06 12:03:59 -0500

    Fetch products in Shop.
    Destructure the object
    properties passed down
    from Home's Outlet context
    with useOutletContext.
    
    In the useEffect, after
    fetching the products,
    add the quantitySelected
    and quantityInCart
    properties and values
    to every product.
    These would be used
    to handle the number
    of products selected
    and the number of
    products added to
    cart.
    
    Call the
    setProductsList on the
    new object.
    
    Log the error to the
    console if there was
    any error.
    
    As products is
    fetching,
    (!productsList), return
    a h1 with a message.
    
    After setProductsList
    is called with the
    new object,
    productList is set
    in Home component
    and it rerenders and
    passes the updated
    products list to
    Shop. Shop now
    renders each product's
    title for now.

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -1,9 +1,56 @@
+import { useEffect } from "react";
 import { Link } from "react-router";
+import { useOutletContext } from "react-router";
 
 const Shop = () => {
+  const {
+    productsList,
+    handleProductsList,
+    handleQuantityChange,
+    handleQuantityInput,
+    handleAddToCart,
+  } = useOutletContext();
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const productsResponse = await fetch(
+          "https://fakestoreapi.com/products",
+          { mode: "cors" }
+        );
+
+        if (!productsResponse.ok) {
+          throw new Error(`Error fetching species: ${productsResponse.status}`);
+        }
+
+        const allProducts = await await productsResponse.json();
+
+        const productsWithQuantities = allProducts.map((product) => ({
+          ...product,
+          quantitySelected: 0,
+          quantityInCart: 0,
+        }));
+
+        handleProductsList(productsWithQuantities);
+        console.log(productsWithQuantities);
+      } catch (error) {
+        console.log("Error:", error);
+      }
+    };
+
+    fetchProducts();
+  }, [handleProductsList]);
+
+  if (!productsList) {
+    return <h1>fetching products...</h1>;
+  }
+
   return (
     <div>
       <h3>Hello I am Shop. Welcome to Shop</h3>
+      {productsList.map((product) => (
+        <div key={product.id}>{product.title}</div>
+      ))}
       <Link to="/">Back Home</Link>
     </div>
   );
