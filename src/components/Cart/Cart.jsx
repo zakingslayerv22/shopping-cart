69d42fcb0e7b779a146e5eb0d842381557c88aa2
Eriwe-li <11926638+zakingslayerv22@users.noreply.github.com>
2026-01-10 12:20:12 -0500

    Setup Cart
    Destructure the object
    properties passed down
    from Home's Outlet context
    with useOutletContext.
    
    Return a message if there
    is no productsList.
    
    If there is productsList,
    in a section, map through
    it and for all products
    that have productsInCart,
    display their image, title,
    description, price with
    counter buttons and
    quantityInCart in
    respective elements.
    Calculate the total
    price of
    quantityInCart \* price.
    Include a remove from
    cart button.

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,10 +1,47 @@
-import { Link } from "react-router";
+import { Link, useOutletContext } from "react-router";
 
 const Cart = () => {
+  const { productsList } = useOutletContext();
+
+  if (!productsList) {
+    return (
+      <h2>No products in cart yet. Add products to cart first to continue.</h2>
+    );
+  }
+
   return (
     <div>
+      {console.log(productsList)}
       <h3>Hello, I am Cart. You can add things to the cart!</h3>
       <Link to="/">Back Home</Link>
+
+      <section>
+        {productsList.map((product) => {
+          if (!product.quantityInCart) return;
+
+          return (
+            <div key={product.id}>
+              <div>
+                <img src={product.image} />
+              </div>
+
+              <div>
+                <p>{product.title}</p>
+                <p>{product.description}</p>
+              </div>
+
+              <p>${product.price}</p>
+              <div>
+                <button>+</button>
+                <input value={product.quantityInCart} />
+                <button>-</button>
+              </div>
+              <p>${product.quantityInCart * product.price}</p>
+              <button>Remove</button>
+            </div>
+          );
+        })}
+      </section>
     </div>
   );
 };
