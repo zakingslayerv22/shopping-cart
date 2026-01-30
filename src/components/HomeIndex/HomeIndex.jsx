ef53f27d2447ef8ffed5f9b206994a663e430067
Eriwe-li <11926638+zakingslayerv22@users.noreply.github.com>
2026-01-30 14:45:41 -0500

    Create an index route
    In a new HomeIndex directory
    create and new jsx file
    with the same name
    and export a component
    that returns a simple
    text.
    
    In routes.jsx, set this new
    HomeIndex as the index
    route of Home.

---
+++ src/components/HomeIndex/HomeIndex.jsx
@@ -0,0 +1,5 @@
+const Default = () => {
+  return <h3>This is home.</h3>;
+};
+
+export default Default;
--- src/config/routes.jsx
+++ src/config/routes.jsx
@@ -2,6 +2,7 @@ import Home from "../components/Home/Home";
 import Shop from "../components/Shop/Shop";
 import Cart from "../components/Cart/Cart";
 import ErrorPage from "../components/ErrorPage/ErrorPage";
+import HomeIndex from "../components/HomeIndex/HomeIndex";
 
 const routes = [
   {
@@ -9,6 +10,7 @@ const routes = [
     element: <Home />,
     errorElement: <ErrorPage />,
     children: [
+      { index: true, element: <HomeIndex /> },
       {
         path: "shop",
         element: <Shop />,
